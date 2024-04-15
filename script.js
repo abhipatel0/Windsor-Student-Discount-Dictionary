2a90588223ce0f731edfb7e1846ebc866516ecb4
Abhi Patel <78019173+abhipatel0@users.noreply.github.com>
2024-04-14 23:59:38 -0400

    added js for maps and chaged logo and aboutUs page

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    function setDestination() {
+        var destination = document.querySelector('.store-name').textContent;
+        document.querySelector('.daddr').value = destination;
+    }
+
+    document.querySelector('.directionForm').addEventListener('submit', setDestination);
+});
